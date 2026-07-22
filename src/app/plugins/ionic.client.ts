13fe2e4d97b7feeab006be0e01ddf2147fa72f99
winardouff <winardo.ist@unifam.com>
2026-07-22 17:29:12 +0700

    patch ionic client ts

--- src/app/plugins/ionic.client.ts
+++ src/app/plugins/ionic.client.ts
@@ -1,7 +1,18 @@
 import { IonicVue } from '@ionic/vue'
 import { defineCustomElements as PwaLoader } from '@ionic/pwa-elements/loader'
 
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(IonicVue)
-  PwaLoader(window)
+// Import Ionic Vue CSS utilities
+import '@ionic/vue/css/core.css'
+import '@ionic/vue/css/normalize.css'
+import '@ionic/vue/css/structure.css'
+import '@ionic/vue/css/typography.css'
+import '@ionic/vue/css/ionic.bundle.css'
+
+export default defineNuxtPlugin({ 
+  name: 'ionic',
+  enforce: 'pre',
+  setup(nuxtApp) {
+    nuxtApp.vueApp.use(IonicVue)
+    PwaLoader(window)
+  }
 })
